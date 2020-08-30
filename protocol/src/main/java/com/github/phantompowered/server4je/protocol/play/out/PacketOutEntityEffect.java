d6c0047947bb18079c675b4ead9255e4843b2c55
derklaro <pasqual.koschmieder@gmail.com>
2020-08-30 20:39:12 +0200

    Implement missing 'EntityEffect' Packet. Closes #152

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutEntityEffect.java
@@ -0,0 +1,135 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.defaults.PrimitivePacket;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.bukkit.entity.Entity;
+import org.bukkit.potion.PotionEffect;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutEntityEffect extends PrimitivePacket {
+
+    private int entityId;
+    private int effectId;
+    private byte amplifier;
+    private int duration;
+    private byte flags;
+
+    public PacketOutEntityEffect() {
+    }
+
+    public PacketOutEntityEffect(Entity entity, PotionEffect effect) {
+        this.entityId = entity.getEntityId();
+        this.effectId = effect.getType().getId();
+        this.amplifier = (byte) effect.getAmplifier();
+        this.duration = effect.getDuration();
+
+        this.flags = 0;
+        if (effect.isAmbient()) {
+            this.flags = (byte) (this.flags | 1);
+        }
+
+        if (effect.hasParticles()) {
+            this.flags = (byte) (this.flags | 2);
+        }
+
+        if (effect.hasIcon()) {
+            this.flags = (byte) (this.flags | 4);
+        }
+    }
+
+    public PacketOutEntityEffect(int entityId, int effectId, byte amplifier, int duration, byte flags) {
+        this.entityId = entityId;
+        this.effectId = effectId;
+        this.amplifier = amplifier;
+        this.duration = duration;
+        this.flags = flags;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeVarInt(this.entityId);
+        dataBuffer.writeByte(this.effectId);
+        dataBuffer.writeByte(this.amplifier);
+        dataBuffer.writeVarInt(this.duration);
+        dataBuffer.writeByte(this.flags);
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutEntityEffect.class);
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public int getEffectId() {
+        return effectId;
+    }
+
+    public void setEffectId(int effectId) {
+        this.effectId = effectId;
+    }
+
+    public byte getAmplifier() {
+        return amplifier;
+    }
+
+    public void setAmplifier(byte amplifier) {
+        this.amplifier = amplifier;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public byte getFlags() {
+        return flags;
+    }
+
+    public void setFlags(byte flags) {
+        this.flags = flags;
+    }
+}
