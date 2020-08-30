e049246804f42f7569e668967b08f7aa1e0fc353
derklaro <pasqual.koschmieder@gmail.com>
2020-08-30 19:53:11 +0200

    Implement missing 'UpdateHealth' Packet. Closes #136

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutUpdateHealth.java
@@ -0,0 +1,93 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.defaults.PrimitivePacket;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutUpdateHealth extends PrimitivePacket {
+
+    // 0 = dead, 20 = full
+    private @Range(from = 0, to = 20) float health;
+    private @Range(from = 0, to = 20) int food;
+    // TODO: recheck if 0 - 5
+    private @Range(from = 0, to = 5) float foodSaturation;
+
+    public PacketOutUpdateHealth() {
+    }
+
+    public PacketOutUpdateHealth(@Range(from = 0, to = 20) float health, @Range(from = 0, to = 20) int food, @Range(from = 0, to = 5) float foodSaturation) {
+        this.health = health;
+        this.food = food;
+        this.foodSaturation = foodSaturation;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeFloat(this.health);
+        dataBuffer.writeVarInt(this.food);
+        dataBuffer.writeFloat(this.foodSaturation);
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getPacketId(ProtocolState.PLAY, PacketOutUpdateHealth.class);
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public float getFoodSaturation() {
+        return foodSaturation;
+    }
+
+    public void setFoodSaturation(float foodSaturation) {
+        this.foodSaturation = foodSaturation;
+    }
+}
