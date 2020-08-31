7b5ac9a17baa0e63490aeba1489f55e9cde0b2f9
derklaro <pasqual.koschmieder@gmail.com>
2020-08-31 14:14:51 +0200

    Implement missing 'BookEdit' Packet. Closes #20

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/in/PacketInBookEdit.java
@@ -0,0 +1,89 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.in;
+
+import com.github.phantompowered.server4je.protocol.Packet;
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyFromClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketInBookEdit implements Packet {
+
+    private ItemStack itemStack;
+    private boolean isSigning;
+    private PacketInArmAnimation.Hand hand;
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        // this.itemStack = dataBuffer.readItemStack();
+        this.isSigning = dataBuffer.readBoolean();
+        this.hand = PacketInArmAnimation.Hand.values()[dataBuffer.readVarInt()];
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        PacketOnlyFromClientException.throwNow();
+    }
+
+    @Override
+    public void releaseData() {
+        this.itemStack = null;
+        this.hand = null;
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getClientPacketId(ProtocolState.PLAY, PacketInBookEdit.class);
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public boolean isSigning() {
+        return isSigning;
+    }
+
+    public void setSigning(boolean signing) {
+        isSigning = signing;
+    }
+
+    public PacketInArmAnimation.Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(PacketInArmAnimation.Hand hand) {
+        this.hand = hand;
+    }
+}
