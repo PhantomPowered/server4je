06bdb545e8a2270b31a3c674045385e3682034a2
derklaro <pasqual.koschmieder@gmail.com>
2020-08-31 15:36:57 +0200

    Implement missing 'ClickWindowItem' Packet. Closes #16

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/in/PacketInClickWindowButton.java
@@ -0,0 +1,75 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.in;
+
+import com.github.phantompowered.server4je.common.annotation.Note;
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.defaults.PrimitivePacket;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyFromClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+@Note("Until 1.14 it looks like this packet is only used for enchantment tables")
+public class PacketInClickWindowButton extends PrimitivePacket {
+
+    private int windowId;
+    @Note("100+ is used for lectern describing the opened page number")
+    private int buttonId;
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        this.windowId = dataBuffer.readByte();
+        this.buttonId = dataBuffer.readByte();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        PacketOnlyFromClientException.throwNow();
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getClientPacketId(ProtocolState.PLAY, PacketInClickWindowButton.class);
+    }
+
+    public int getWindowId() {
+        return windowId;
+    }
+
+    public void setWindowId(int windowId) {
+        this.windowId = windowId;
+    }
+
+    public int getButtonId() {
+        return buttonId;
+    }
+
+    public void setButtonId(int buttonId) {
+        this.buttonId = buttonId;
+    }
+}
