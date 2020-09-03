1d2fc6f4b148f7c4a33d5ef860c06a46a981696b
derklaro <pasqual.koschmieder@gmail.com>
2020-09-03 21:23:49 +0200

    Implement missing 'CollectItem' Packet. Closes #148

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutCollectItem.java
@@ -0,0 +1,91 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.defaults.PrimitivePacket;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutCollectItem extends PrimitivePacket {
+
+    private int collectedEntity;
+    private int collectingEntity;
+    private int itemCount;
+
+    public PacketOutCollectItem() {
+    }
+
+    public PacketOutCollectItem(int collectedEntity, int collectingEntity, int itemCount) {
+        this.collectedEntity = collectedEntity;
+        this.collectingEntity = collectingEntity;
+        this.itemCount = itemCount;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeVarInt(this.collectedEntity);
+        dataBuffer.writeVarInt(this.collectedEntity);
+        dataBuffer.writeVarInt(this.itemCount);
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutCollectItem.class);
+    }
+
+    public int getCollectedEntity() {
+        return this.collectedEntity;
+    }
+
+    public void setCollectedEntity(int collectedEntity) {
+        this.collectedEntity = collectedEntity;
+    }
+
+    public int getCollectingEntity() {
+        return this.collectingEntity;
+    }
+
+    public void setCollectingEntity(int collectingEntity) {
+        this.collectingEntity = collectingEntity;
+    }
+
+    public int getItemCount() {
+        return this.itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+}
