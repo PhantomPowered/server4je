4f91874d83b9dc86566f5e36d5097c7196bb325e
derklaro <pasqual.koschmieder@gmail.com>
2020-09-03 20:31:50 +0200

    Implement missing 'BlockBreakAnimation' Packet. Closes #71

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutBlockBreakAnimation.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.common.annotation.Note;
+import com.github.phantompowered.server4je.protocol.Packet;
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import com.github.phantompowered.server4je.protocol.utils.LocationUtil;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutBlockBreakAnimation implements Packet {
+
+    private int entityId;
+    private Location blockLocation;
+    @Note("0–9 to set it, any other value to remove it")
+    private int destroyState;
+
+    public PacketOutBlockBreakAnimation() {
+    }
+
+    public PacketOutBlockBreakAnimation(int entityId, Location blockLocation, @Note("0–9 to set it, any other value to remove it") int destroyState) {
+        this.entityId = entityId;
+        this.blockLocation = blockLocation;
+        this.destroyState = destroyState;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeVarInt(this.entityId);
+        dataBuffer.writeLong(LocationUtil.locationToLong(this.blockLocation));
+        dataBuffer.writeByte(this.destroyState);
+    }
+
+    @Override
+    public void releaseData() {
+        this.blockLocation = null;
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutBlockBreakAnimation.class);
+    }
+
+    public int getEntityId() {
+        return this.entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public Location getBlockLocation() {
+        return this.blockLocation;
+    }
+
+    public void setBlockLocation(Location blockLocation) {
+        this.blockLocation = blockLocation;
+    }
+
+    public int getDestroyState() {
+        return this.destroyState;
+    }
+
+    public void setDestroyState(int destroyState) {
+        this.destroyState = destroyState;
+    }
+}
