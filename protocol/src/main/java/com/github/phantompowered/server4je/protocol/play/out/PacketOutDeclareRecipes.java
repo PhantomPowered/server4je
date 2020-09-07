4f57e93df41bc71d4314045948421e153bc7988e
derklaro <pasqual.koschmieder@gmail.com>
2020-09-07 14:06:18 +0200

    Implement missing 'DeclareRecipes' Packet. Closes #153

--- protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutDeclareRecipes.java
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutDeclareRecipes.java
@@ -24,45 +24,26 @@
  */
 package com.github.phantompowered.server4je.protocol.play.out;
 
-import com.destroystokyo.paper.Namespaced;
-import com.github.phantompowered.server4je.common.enums.EnumUtil;
 import com.github.phantompowered.server4je.protocol.Packet;
 import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
 import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
 import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
 import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
-import com.github.phantompowered.server4je.protocol.play.in.PacketInSetRecipeBookState;
 import com.github.phantompowered.server4je.protocol.state.ProtocolState;
-import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.function.Consumer;
 
 public class PacketOutDeclareRecipes implements Packet {
 
-    private Action action;
-    private Collection<Namespaced> recipes;
-    private Collection<Namespaced> displayedRecipes;
-    private Map<PacketInSetRecipeBookState.RecipeBookType, RecipeData> recipeDataMap;
+    private Consumer<DataBuffer> recipeSerializer;
 
     public PacketOutDeclareRecipes() {
     }
 
-    public PacketOutDeclareRecipes(Action action, Collection<Namespaced> recipes, Map<PacketInSetRecipeBookState.RecipeBookType, RecipeData> recipeDataMap) {
-        Preconditions.checkArgument(action == Action.INIT);
-        this.action = action;
-        this.recipes = recipes;
-        this.recipeDataMap = recipeDataMap;
-    }
-
-    public PacketOutDeclareRecipes(Action action, Collection<Namespaced> recipes,
-                                   Collection<Namespaced> displayedRecipes, Map<PacketInSetRecipeBookState.RecipeBookType, RecipeData> recipeDataMap) {
-        this.action = action;
-        this.recipes = recipes;
-        this.displayedRecipes = displayedRecipes;
-        this.recipeDataMap = recipeDataMap;
+    public PacketOutDeclareRecipes(Consumer<DataBuffer> recipeSerializer) {
+        this.recipeSerializer = recipeSerializer;
     }
 
     @Override
@@ -72,38 +53,12 @@ public class PacketOutDeclareRecipes implements Packet {
 
     @Override
     public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
-        dataBuffer.writeVarInt(this.action.ordinal());
-
-        for (PacketInSetRecipeBookState.RecipeBookType entry : EnumUtil.getEnumEntries(PacketInSetRecipeBookState.RecipeBookType.class)) {
-            RecipeData recipeData = this.recipeDataMap.get(entry);
-            if (recipeData == null) {
-                dataBuffer.writeBoolean(Boolean.FALSE);
-                dataBuffer.writeBoolean(Boolean.FALSE);
-            } else {
-                dataBuffer.writeBoolean(recipeData.open);
-                dataBuffer.writeBoolean(recipeData.filtering);
-            }
-        }
-
-        dataBuffer.writeVarInt(this.recipes.size());
-        for (Namespaced recipe : this.recipes) {
-            dataBuffer.writeNamespaced(recipe);
-        }
-
-        if (this.action == Action.INIT) {
-            dataBuffer.writeVarInt(this.displayedRecipes.size());
-            for (Namespaced displayedRecipe : this.displayedRecipes) {
-                dataBuffer.writeNamespaced(displayedRecipe);
-            }
-        }
+        this.recipeSerializer.accept(dataBuffer);
     }
 
     @Override
     public void releaseData() {
-        this.action = null;
-        this.recipes = null;
-        this.displayedRecipes = null;
-        this.recipeDataMap = null;
+        this.recipeSerializer = null;
     }
 
     @Override
@@ -111,61 +66,11 @@ public class PacketOutDeclareRecipes implements Packet {
         return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutDeclareRecipes.class);
     }
 
-    public Action getAction() {
-        return this.action;
+    public Consumer<DataBuffer> getRecipeSerializer() {
+        return this.recipeSerializer;
     }
 
-    public void setAction(Action action) {
-        this.action = action;
-    }
-
-    public Collection<Namespaced> getRecipes() {
-        return this.recipes;
-    }
-
-    public void setRecipes(Collection<Namespaced> recipes) {
-        this.recipes = recipes;
-    }
-
-    public Collection<Namespaced> getDisplayedRecipes() {
-        return this.displayedRecipes;
-    }
-
-    public void setDisplayedRecipes(Collection<Namespaced> displayedRecipes) {
-        this.displayedRecipes = displayedRecipes;
-    }
-
-    public Map<PacketInSetRecipeBookState.RecipeBookType, RecipeData> getRecipeDataMap() {
-        return this.recipeDataMap;
-    }
-
-    public void setRecipeDataMap(Map<PacketInSetRecipeBookState.RecipeBookType, RecipeData> recipeDataMap) {
-        this.recipeDataMap = recipeDataMap;
-    }
-
-    public enum Action {
-
-        INIT,
-        ADD,
-        REMOVE
-    }
-
-    public static class RecipeData {
-
-        private boolean open;
-        private boolean filtering;
-
-        public RecipeData(boolean open, boolean filtering) {
-            this.open = open;
-            this.filtering = filtering;
-        }
-
-        public void setOpen(boolean open) {
-            this.open = open;
-        }
-
-        public void setFiltering(boolean filtering) {
-            this.filtering = filtering;
-        }
+    public void setRecipeSerializer(Consumer<DataBuffer> recipeSerializer) {
+        this.recipeSerializer = recipeSerializer;
     }
 }
