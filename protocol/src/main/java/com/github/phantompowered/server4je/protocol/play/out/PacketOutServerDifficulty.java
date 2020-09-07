a99c288dfb102b1485296ff66846ae1d22662748
derklaro <pasqual.koschmieder@gmail.com>
2020-09-07 21:43:05 +0200

    Implement missing 'ServerDifficulty' Packet. Closes #76

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutServerDifficulty.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.protocol.Packet;
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.bukkit.Difficulty;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutServerDifficulty implements Packet {
+
+    private Difficulty difficulty;
+    private boolean locked;
+
+    public PacketOutServerDifficulty() {
+    }
+
+    public PacketOutServerDifficulty(Difficulty difficulty) {
+        this(difficulty, Boolean.TRUE);
+    }
+
+    public PacketOutServerDifficulty(Difficulty difficulty, boolean locked) {
+        this.difficulty = difficulty;
+        this.locked = locked;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeByte(this.difficulty.ordinal());
+        dataBuffer.writeBoolean(this.locked);
+    }
+
+    @Override
+    public void releaseData() {
+        this.difficulty = null;
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutServerDifficulty.class);
+    }
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public boolean isLocked() {
+        return this.locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+}
