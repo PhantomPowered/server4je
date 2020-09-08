6144e0ebad62851cc5607c228c71e4c4ce5929b4
derklaro <pasqual.koschmieder@gmail.com>
2020-09-08 12:22:24 +0200

    Implement missing 'TabComplete' Packet. Closes #78

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutTabComplete.java
@@ -0,0 +1,99 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.protocol.Packet;
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import com.mojang.brigadier.suggestion.Suggestion;
+import com.mojang.brigadier.suggestion.Suggestions;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutTabComplete implements Packet {
+
+    private int transactionId;
+    private Suggestions suggestions;
+
+    public PacketOutTabComplete() {
+    }
+
+    public PacketOutTabComplete(int transactionId, Suggestions suggestions) {
+        this.transactionId = transactionId;
+        this.suggestions = suggestions;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeVarInt(this.transactionId);
+        dataBuffer.writeVarInt(this.suggestions.getRange().getStart());
+        dataBuffer.writeVarInt(this.suggestions.getRange().getLength());
+
+        dataBuffer.writeVarInt(this.suggestions.getList().size());
+        for (Suggestion suggestion : this.suggestions.getList()) {
+            dataBuffer.writeString(suggestion.getText());
+            dataBuffer.writeBoolean(suggestion.getTooltip() != null);
+            if (suggestion.getTooltip() != null) {
+                dataBuffer.writeString(ComponentSerializer.toString(TextComponent.fromLegacyText(suggestion.getTooltip().getString())));
+            }
+        }
+    }
+
+    @Override
+    public void releaseData() {
+        this.suggestions = null;
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutTabComplete.class);
+    }
+
+    public int getTransactionId() {
+        return this.transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Suggestions getSuggestions() {
+        return this.suggestions;
+    }
+
+    public void setSuggestions(Suggestions suggestions) {
+        this.suggestions = suggestions;
+    }
+}
