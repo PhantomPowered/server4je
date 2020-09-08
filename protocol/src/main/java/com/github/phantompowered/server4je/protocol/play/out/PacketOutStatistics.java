380fcc3a58c743db688a1b297699e6b7f04611e2
derklaro <pasqual.koschmieder@gmail.com>
2020-09-08 12:14:35 +0200

    Implement missing 'Statistics' Packet. Closes #69

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutStatistics.java
@@ -0,0 +1,80 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.api.statistic.StatisticWrapper;
+import com.github.phantompowered.server4je.protocol.Packet;
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import it.unimi.dsi.fastutil.objects.ObjectSet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutStatistics implements Packet {
+
+    private ObjectSet<StatisticWrapper> statisticWrappers;
+
+    public PacketOutStatistics() {
+    }
+
+    public PacketOutStatistics(ObjectSet<StatisticWrapper> statisticWrappers) {
+        this.statisticWrappers = statisticWrappers;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        for (StatisticWrapper statisticWrapper : this.statisticWrappers) {
+            dataBuffer.writeVarInt(statisticWrapper.getCategory().ordinal());
+            dataBuffer.writeVarInt(statisticWrapper.getId());
+            dataBuffer.writeVarInt(statisticWrapper.intValue());
+        }
+    }
+
+    @Override
+    public void releaseData() {
+        this.statisticWrappers = null;
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutStatistics.class);
+    }
+
+    public ObjectSet<StatisticWrapper> getStatisticWrappers() {
+        return this.statisticWrappers;
+    }
+
+    public void setStatisticWrappers(ObjectSet<StatisticWrapper> statisticWrappers) {
+        this.statisticWrappers = statisticWrappers;
+    }
+}
