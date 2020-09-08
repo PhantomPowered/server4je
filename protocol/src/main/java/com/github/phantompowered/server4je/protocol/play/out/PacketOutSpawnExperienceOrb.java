2b291a487a1c1f49bae1e5b6bf014805b55c5d60
derklaro <pasqual.koschmieder@gmail.com>
2020-09-08 11:21:40 +0200

    Implement missing 'SpawnExperienceOrb' Packet. Closes #59

---
+++ protocol/src/main/java/com/github/phantompowered/server4je/protocol/play/out/PacketOutSpawnExperienceOrb.java
@@ -0,0 +1,122 @@
+/*
+ * This file is part of server4je, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) PhantomPowered <https://github.com/PhantomPowered>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.phantompowered.server4je.protocol.play.out;
+
+import com.github.phantompowered.server4je.protocol.annotation.BufferStatus;
+import com.github.phantompowered.server4je.protocol.buffer.DataBuffer;
+import com.github.phantompowered.server4je.protocol.defaults.PrimitivePacket;
+import com.github.phantompowered.server4je.protocol.exceptions.PacketOnlyToClientException;
+import com.github.phantompowered.server4je.protocol.id.PacketIdUtil;
+import com.github.phantompowered.server4je.protocol.state.ProtocolState;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class PacketOutSpawnExperienceOrb extends PrimitivePacket {
+
+    private int entityId;
+    private double x;
+    private double y;
+    private double z;
+    private short count;
+
+    public PacketOutSpawnExperienceOrb() {
+    }
+
+    public PacketOutSpawnExperienceOrb(int entityId, @NotNull Location location, short count) {
+        this.entityId = entityId;
+        this.x = location.getX();
+        this.y = location.getY();
+        this.z = location.getZ();
+        this.count = count;
+    }
+
+    public PacketOutSpawnExperienceOrb(int entityId, double x, double y, double z, short count) {
+        this.entityId = entityId;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.count = count;
+    }
+
+    @Override
+    public void readData(@NotNull @BufferStatus(BufferStatus.Status.FILLED) DataBuffer dataBuffer) {
+        PacketOnlyToClientException.throwNow();
+    }
+
+    @Override
+    public void writeData(@NotNull @BufferStatus(BufferStatus.Status.EMPTY) DataBuffer dataBuffer) {
+        dataBuffer.writeVarInt(this.entityId);
+        dataBuffer.writeDouble(this.x);
+        dataBuffer.writeDouble(this.y);
+        dataBuffer.writeDouble(this.z);
+        dataBuffer.writeShort(this.count);
+    }
+
+    @Override
+    public @Range(from = 0, to = Short.MAX_VALUE) short getId() {
+        return PacketIdUtil.getServerPacketId(ProtocolState.PLAY, PacketOutSpawnExperienceOrb.class);
+    }
+
+    public int getEntityId() {
+        return this.entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public short getCount() {
+        return this.count;
+    }
+
+    public void setCount(short count) {
+        this.count = count;
+    }
+}
